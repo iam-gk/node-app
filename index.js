e284daf4f6efc996da60c0cf3fbfc810567ad301
Gaurav <gauravkr6251@gmail.com>
2023-10-01 20:55:38 +0530

    .gitignore changes

---
+++ index.js
@@ -0,0 +1,58 @@
+const lib = require('./lib.js')
+const express = require('express');
+
+console.log('hello world');
+console.log('hello');
+// console.log('hello');
+
+const server = express();
+server.listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {sum,diff} from './lib.js'
+// const fs = require('fs');
+
+// const t1 = performance.now();
+
+// const txt = fs.readFileSync('demo.txt','utf-8');
+
+
+// fs.readFile('demo.txt','utf-8', (error, txt)=>{
+//     console.log(txt);
+// });
+// console.log(txt);
+
+// console.log(lib.sum(4,5),lib.diff(3,6))
+// const t2 = performance.now();
+// console.log(t2-t1);
+// const a=5;
+
+
+
